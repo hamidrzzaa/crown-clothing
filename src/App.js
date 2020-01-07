6bca84941866379d4cf1440011db1bef547eeed8
hamidrzzaa <hamidrzaa11@gmail.com>
2020-01-07 14:49:05 +0330

    the nav bar is ready

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import "./App.style.css";
 import HomePage from "./Pages/HomePage/HomePage";
 import ShopPage from "./Pages/ShopPage/ShopPage";
+import Header from "./components/Header/Header";
+
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/shop" component={ShopPage} />
