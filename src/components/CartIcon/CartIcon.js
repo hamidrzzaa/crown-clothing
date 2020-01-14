4c3e00db0bda76fe258aad6568f99c1747f56404
hamidrzzaa <hamidrzaa11@gmail.com>
2020-01-14 14:14:19 +0330

    memoize cartIcon component

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { memo } from "react";
+import { compose } from "redux";
 import { connect } from "react-redux";
 import { toggleCartMenu } from "../../actions/cartActions";
 import { ReactComponent as ShoppingIcon } from "../../assets/11.1 shopping-bag.svg.svg";
 import "./CartIcon.styles.scss";
 
-const CartIcon = ({ toggleCartMenu }) => {
+const CartIcon = memo(({ toggleCartMenu, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartMenu}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
-};
+});
 
-export default connect(null, { toggleCartMenu })(CartIcon);
+const mapStateToProps = state => {
+  console.log();
+  return {
+    itemCount: state.cart.cartItems.reduce(
+      (sum, cartItem) => sum + cartItem.quantity,
+      0
+    )
+  };
+};
+compose();
+export default connect(mapStateToProps, { toggleCartMenu })(CartIcon);
